7f078fe45df4502686866294048e562232dcaa6f
Aki Gao <xag9bb@virginia.edu>
2017-06-26 22:42:59 -0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+    },
+  },
+  plugins: ['node'],
+  extends: ['eslint:recommended', 'plugin:node/recommended'],
+  rules: {
+    'node/no-unsupported-features': 0,
+    'node/exports-style': ['error', 'module.exports'],
+    'no-console': 0,
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+  env: {
+    node: true,
+  },
+};
---
+++ README.md
@@ -0,0 +1,12 @@
+# Node Template
+A Tinder bot to automatically swipe right on local profiles.
+
+#### To use
+Checkout this repo, install dependencies, then run the following commands in the terminal.
+
+```
+> git clone https://github.com/iknowhtml/tinder-bot.git
+> cd tinder-bot
+> yarn install
+> yarn start
+```
