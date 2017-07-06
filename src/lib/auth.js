e85cf93f9f9960ac69574c84797b458df9a2d5cd
iknowhtml <iknowsmtp@gmail.com>
2017-07-06 10:24:12 -0400

    successfully add functionality to login to facebook account using phantomjs

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,5 @@
 import phantom from 'phantom';
-// import { FB_EMAIL, FB_PASSWORD } from './fb_login_credentials';
+import credentials from './fb_login_credentials';
 
 const FB_AUTH_TOKEN_URL =
   'https://www.facebook.com/v2.6/dialog/oauth?redirect_uri=fb464891386855067%3A%2F%2Fauthorize%2F&state=%7B%22challenge%22%3A%22q1WMwhvSfbWHvd8xz5PT6lk6eoA%253D%22%2C%220_auth_logger_id%22%3A%2254783C22-558A-4E54-A1EE-BB9E357CC11F%22%2C%22com.facebook.sdk_client_state%22%3Atrue%2C%223_method%22%3A%22sfvc_auth%22%7D&scope=user_birthday%2Cuser_photos%2Cuser_education_history%2Cemail%2Cuser_relationship_details%2Cuser_friends%2Cuser_work_history%2Cuser_likes&response_type=token%2Csigned_request&default_audience=friends&return_scopes=true&auth_type=rerequest&client_id=464891386855067&ret=login&sdk=ios&logger_id=54783C22-558A-4E54-A1EE-BB9E357CC11F#_=_';
@@ -8,12 +8,22 @@ async function getAuthenticationToken() {
   try {
     const instance = await phantom.create();
     const page = await instance.createPage();
-    await page.on('onResourceRequested', requestData => {
-      console.info(requestData.url);
+    await page.on('onConsoleMessage', msg => {
+      console.log(msg);
     });
+
     const status = await page.open(FB_AUTH_TOKEN_URL);
-    console.log(status);
-    await instance.exit();
+
+    if (status === 'success') {
+      await page.evaluate(function(credentials) {
+        document.getElementById('email').value = credentials.FB_EMAIL;
+        document.getElementById('pass').value = credentials.FB_PASSWORD;
+        document.getElementById('loginbutton').click();
+      }, credentials);
+
+    } else {
+      console.error('Unable to open URL');
+    }
   } catch (error) {
     console.log(error);
   }
