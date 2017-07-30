19fb956c8dbb0bea27e2fbb5bb31c66074192189
Aki Gao <xag9bb@virginia.edu>
2017-07-30 14:58:37 -0400

    add newly refactored get auth token, start troubleshooting issue with tinder npm library

--- src/app.js
+++ src/app.js
@@ -1,20 +1,45 @@
+import https from 'https';
 import tinder from 'tinder';
-import { FB_USER_ID, FB_USER_TOKEN } from './fb_credentials';
+import getAuthToken from './lib/get_auth_token';
 
-const LIMIT = 100;
+const LIMIT = 10;
 
 const client = new tinder.TinderClient();
-
-client.authorize(FB_USER_TOKEN, FB_USER_ID, () => {
-  client.getRecommendations(LIMIT, (error, { results }) => {
-    results.forEach(({ _id }) => {
-      client.like(_id, (error, data) => {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(data);
-        }
+getAuthToken().then(async userToken => {
+  const userId = await (() =>
+    new Promise(resolve => {
+      const userIdUrl =
+        'https://graph.facebook.com/v2.6/me?fields=id&access_token=';
+      https.get(`${userIdUrl}${userToken}`, res => {
+        res.setEncoding('utf8');
+        let body = '';
+        res.on('data', data => {
+          body += data;
+        });
+        res.on('end', () => {
+          const json = JSON.parse(body);
+          resolve(json.id);
+        });
       });
-    });
+    }))();
+
+  client.authorize(userToken, userId, () => {
+    console.log(client.isAuthorized());
+    // client.getRecommendations(LIMIT, (recomendationsError, { results }) => {
+    //   if (recomendationsError) {
+    //     console.error(recomendationsError);
+    //   } else {
+    //     console.log(results);
+    //     results.forEach(({ _id }) => {
+    //       client.like(_id, (likeError, data) => {
+    //         if (likeError) {
+    //           console.error(likeError);
+    //         } else {
+    //           console.log(data);
+    //         }
+    //       });
+    //     });
+    //   }
+    // });
   });
 });
